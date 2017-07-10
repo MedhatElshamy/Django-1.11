2c95978b980a7fe234a823a7faa68308fbae599d
Medhat Elshamy <medhat.elshamy7@gmail.com>
2017-07-10 19:47:43 +0300

    Rendering HTML

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 # Function based view
 
 def home(request):
-	return HttpResponse('Hello')
+	html_ = '''<!DOCTYPE html>
+	<html>
+	<head>
+	</head>
+	<body>
+		<h1>Hello From Home!</h1>
+		<p>This Is Testing Page</p>
+	</body>
+	</html>'''
+	return HttpResponse(html_)
